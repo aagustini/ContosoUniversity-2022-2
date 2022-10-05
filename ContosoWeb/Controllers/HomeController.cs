60e99115a58c8c3346a6f93a4a344f3ad1c9cfa1
AAgustini <alexandre.agustini@pucrs.br>
2022-10-05 20:13:01 -0300

    Manutencao de estado com cookies

--- ContosoWeb/Controllers/HomeController.cs
+++ ContosoWeb/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
 ﻿using ContosoWeb.Models;
 using ContosoWeb.Models.SchoolViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Persistencia.Entidades;
 using Persistencia.Repositorio;
 using System;
 using System.Collections.Generic;
@@ -24,9 +29,19 @@ namespace ContosoWeb.Controllers
             private readonly ILogger<HomeController> _logger;
             private readonly SchoolContext _context;
 
-            public HomeController(ILogger<HomeController> logger, SchoolContext context)
+        public readonly UserManager<ApplicationUser> _userManager;
+
+        private IWebHostEnvironment _environment;
+
+
+        public HomeController(ILogger<HomeController> logger, 
+                              SchoolContext context,
+                              UserManager<ApplicationUser> userManager,
+                              IWebHostEnvironment environment)
             {
                 _logger = logger;
+                _userManager = userManager;
+                _environment = environment;
                 _context = context;
             }
 
@@ -35,8 +50,17 @@ namespace ContosoWeb.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        [Authorize]
+        public async Task<IActionResult> Privacy()
         {
+            var usuario = await _userManager.GetUserAsync(User);
+
+
+            CookieOptions option = new CookieOptions();
+            option.Expires = DateTime.Now.AddSeconds(30);
+            Response.Cookies.Append("UserName", usuario.Email, option);
+            Response.Cookies.Append("DataHora", DateTime.Now.ToString(), option);
+
             return View();
         }
 
@@ -46,8 +70,22 @@ namespace ContosoWeb.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        
         public async Task<ActionResult> About()
         {
+            string userName = Request.Cookies["UserName"];
+            string dataHora = Request.Cookies["DataHora"];
+
+            if (userName != null)
+            {
+                ViewData["usuario"] = userName;
+                ViewData["dataH"] = dataHora;
+            }
+            else
+            {
+                ViewData["usuario"] = "não logado";
+                ViewData["dataH"] =  "";
+            }
             IQueryable<EnrollmentDateGroup> data =
                 from student in _context.Students
                 group student by student.EnrollmentDate into dateGroup
